361dda5454c27327b4894ca76fd757f1a02e7f23
Alex Toofanian <alextoofanian@gmail.com>
2023-04-03 18:49:20 +0000

    Added wrapper functions for dyn system consistency

--- refineNCBF/dynamic_systems/dynamic_systems.py
+++ refineNCBF/dynamic_systems/dynamic_systems.py
@@ -20,7 +20,15 @@ class DynamicSystem(ABC):
     disturbance_lower_bounds: Tuple[float, ...]
     disturbance_upper_bounds: Tuple[float, ...]
 
-    # TODO: wrap state is unused, since there is no "step" function which returns xdot*t + x, just dynamics which returns xdot
+    def __init__(self, **kwargs):
+        self.n_dims = self.state_dimensions
+        self.controls_dims = self.control_dimensions
+        self.disturbance_dims = self.disturbance_dimensions
+        self.periodic_dims = self.periodic_state_dimensions
+
+    def __call__(self, state, control, time):
+        return self.compute_dynamics(state, control, np.zeros((state.shape[0], self.disturbance_dims)))
+
     def wrap_state(self, state: VectorBatch) -> VectorBatch:
         """
 
@@ -35,12 +43,40 @@ class DynamicSystem(ABC):
             )
         return wrapped_state
 
-    def compute_dynamics(
-            self,
-            state: VectorBatch,
-            control: VectorBatch,
-            disturbance: VectorBatch
-    ) -> VectorBatch:
+    def wrap_dynamics(self, state: np.ndarray) -> np.ndarray:
+        """
+        Periodic dimensions are wrapped to [-pi, pi)
+        Args:
+            state (np.ndarray): Unwrapped state
+        Returns:
+            state (np.ndarray): Wrapped state
+        """
+        for periodic_dim in self.periodic_dims:
+            try:
+                state[..., periodic_dim] = (state[..., periodic_dim] + np.pi) % (2 * np.pi) - np.pi
+            except TypeError:  # FIXME: Clunky at best, how to deal with jnp and np mix
+                state = state.at[periodic_dim].set((state[periodic_dim] + np.pi) % (2 * np.pi) - np.pi)
+
+        return state
+
+    def step(self, state: np.ndarray, control: np.ndarray, time: float = 0.0, scheme: str = "fe") -> np.ndarray:
+        """Implements the discrete-time dynamics ODE
+        scheme in {fe, rk4}"""
+        if scheme == "fe":
+            n_state = state + self(state, control, time) * self.dt
+        elif scheme == "rk4":
+            # TODO: Figure out how to do RK4 with periodic dimensions (aka angle normalization)
+            # Assumes zoh on control
+            k1 = self(state, control, time)
+            k2 = self(state + k1 * self.dt / 2, control, time + self.dt / 2)
+            k3 = self(state + k2 * self.dt / 2, control, time + self.dt / 2)
+            k4 = self(state + k3 * self.dt, control, time + self.dt)
+            n_state = state + (k1 + 2 * k2 + 2 * k3 + k4) * self.dt / 6
+        else:
+            raise ValueError("scheme must be either 'fe' or 'rk4'")
+        return self.wrap_dynamics(n_state)
+
+    def compute_dynamics(self, state: VectorBatch, control: VectorBatch, disturbance: VectorBatch) -> VectorBatch:
         assert state.shape[1] == self.state_dimensions
         assert control.shape[1] == self.control_dimensions
         assert disturbance.shape[1] == self.disturbance_dimensions
@@ -49,10 +85,10 @@ class DynamicSystem(ABC):
 
     @abstractmethod
     def _compute_dynamics(
-            self,
-            state: VectorBatch,
-            control: VectorBatch,
-            disturbance: VectorBatch,
+        self,
+        state: VectorBatch,
+        control: VectorBatch,
+        disturbance: VectorBatch,
     ) -> VectorBatch:
         ...
 
@@ -60,10 +96,10 @@ class DynamicSystem(ABC):
 @attr.s(auto_attribs=True, eq=False)
 class ControlAffineDynamicSystem(DynamicSystem, ABC):
     def _compute_dynamics(
-            self,
-            state: VectorBatch,
-            control: VectorBatch,
-            disturbance: VectorBatch,
+        self,
+        state: VectorBatch,
+        control: VectorBatch,
+        disturbance: VectorBatch,
     ) -> VectorBatch:
         open_loop_dynamics = self.compute_open_loop_dynamics(state)
         control_jacobian = self.compute_control_jacobian(state)
@@ -73,31 +109,42 @@ class ControlAffineDynamicSystem(DynamicSystem, ABC):
         dynamics = open_loop_dynamics + control_jacobian @ control + disturbance_jacobian @ disturbance
         return dynamics
 
+    def open_loop_dynamics(self, state: np.ndarray, time: float = 0.0):
+        return self.compute_open_loop_dynamics(state)
+
     @abstractmethod
     def compute_open_loop_dynamics(
-            self,
-            state: VectorBatch,
+        self,
+        state: VectorBatch,
     ) -> VectorBatch:
         ...
 
+    def control_jacobian(self, state: np.ndarray, time: float = 0.0):
+        return self.control_matrix(state, time)
+
+    def control_matrix(self, state: np.ndarray, time: float = 0.0):
+        return self.compute_control_jacobian(state)
+
+    def disturbance_jacobian(self, state: np.ndarray, time: float = 0.0):
+        return self.compute_disturbance_jacobian(state)
+
     @abstractmethod
     def compute_control_jacobian(
-            self,
-            state: VectorBatch,
+        self,
+        state: VectorBatch,
     ) -> MatrixBatch:
         ...
 
     @abstractmethod
     def compute_disturbance_jacobian(
-            self,
-            state: VectorBatch,
+        self,
+        state: VectorBatch,
     ) -> MatrixBatch:
         ...
 
 
 @attr.s(auto_attribs=True, eq=False)
 class ControlAffineDynamicSystemFixedPolicy(ControlAffineDynamicSystem, ABC):
-
     @abstractmethod
     def compute_control(self, state: VectorBatch) -> VectorBatch:
         ...
